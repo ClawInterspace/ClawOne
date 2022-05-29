4700b2a195502fb0b1bbb6fc22bd0c04be98c5fe
ClawInterspace <alanliu71104@gmail.com>
2022-05-29 18:04:55 +0800

    basic structure

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
+/* eslint-disable */
 const { defineConfig } = require('@vue/cli-service')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = defineConfig({
+  lintOnSave: process.env.NODE_ENV !== 'development',
   transpileDependencies: true,
   devServer: {
     https: true,
