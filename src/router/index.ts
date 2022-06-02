9f6a266ae2029f87b341c7fc2c33da5430300257
ClawInterspace <alanliu71104@gmail.com>
2022-06-02 14:42:17 +0800

    WIP: deal with main menu => integrate with home

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,23 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Main from '@/views/Main.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
-  }
+    name: 'main',
+    component: () => import(/* webpackChunkName: "main" */ '@/views/Main.vue'),
+    children: [
+      {
+        path: 'home',
+        name: 'home',
+        component: () =>
+          import(/* webpackChunkName: "main" */ '@/views/HomeView.vue'),
+      },
+    ]
+  },
 ]
+
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
